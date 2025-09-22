895dba3c479548d64e61cdd339261bfb898352ae
Jamie Tanna <jamie@jamietanna.co.uk>
2025-09-22 09:52:33 +0100

    chore: refactor `main` into function

--- main.go
+++ main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh/v2/pkg/api"
 )
 
 func main() {
+	if err := run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "gh-discussion failed: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
 	fmt.Println("hi world, this is the gh-discussion extension!")
 	client, err := api.DefaultRESTClient()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
-	response := struct {Login string}{}
+	response := struct{ Login string }{}
 	err = client.Get("user", &response)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 	fmt.Printf("running as %s\n", response.Login)
+
+	return nil
 }
 
 // For more examples of using go-gh, see:
